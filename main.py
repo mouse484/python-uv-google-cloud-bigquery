2c5e0151f7f9a20b9d511289112183d3ee7ecc5b
yuto <yuto@mousedev.page>
2025-06-30 11:21:04 +0900

    BigQueryクライアントの初期化を追加し、データ送信処理を改善

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from python-uv-google-cloud-bigquery!")
+import time
+from gpiozero import MCP3008, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
+from google.cloud import bigquery
+from datetime import datetime
+from typing import TypedDict, List
+
+client = bigquery.Client.from_service_account_json("it_bigquery_secret.json")
+table_ref = client.dataset("yuto_test").table("test_table")
 
 
-if __name__ == "__main__":
-    main()
+moisture_sensor = MCP3008(channel=0)
+
+last_sent = time.time()
+
+
+class MoistureData(TypedDict):
+    time: str
+    value: float
+
+
+rows: List[MoistureData] = []
+
+while True:
+    raw_value = moisture_sensor.value
+    timestamp = datetime.now().isoformat()
+
+    rows.append({"time": timestamp, "value": raw_value})
+
+    print(f"[{timestamp}] {raw_value:3f}")
+
+    if time.time() - last_sent >= 60:
+        errors = client.insert_rows_json(table_ref, rows)
+        if errors == []:
+            print("データ送信成功")
+        else:
+            print(f"データ送信失敗: {errors}")
+        rows.clear()
+        last_sent = time.time()
+
+    time.sleep(1)
